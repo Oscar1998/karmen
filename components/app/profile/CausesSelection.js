e4d384a69e4e48c7cb1aab00047d8e7e19b0abfa
birwas <birwashiwani@hotmail.com>
2020-04-16 10:41:03 +0100

    Implemented the changes done to grid to work in profile edit

--- components/app/profile/CausesSelection.js
+++ components/app/profile/CausesSelection.js
@@ -11,17 +11,69 @@ import Grid from '../../reusables/Grid';
 export default class CausesSelection extends Component {
   constructor(){
     super();
-    this.state={ button1:false, button2:false, button3:false, button4:false, button5:false, button6:false, button7:false, button8:false, button9:false
-  }
+    this.state ={
+      community:false,
+      poverty:false,
+      animals:false,
+      homeless:false,
+      food:false,
+      education:false,
+      health:false,
+      equality:false,
+      religious:false,
+      crisis:false,
+      arts:false,
+      energy:false,
+      conservation:false,
+      peace:false,
+      refugees:false
+
+    }
+    this.causeSelected = this.causeSelected.bind(this)
   }
   
+  causeSelected(name){
+    switch(name){
+      case 'community': this.setState({community:!this.state.community}) 
+      break;
+      case 'poverty': this.setState({poverty:!this.state.poverty})
+      break;
+      case 'animals': this.setState({animals:!this.state.animals})
+      break;
+      case 'homeless': this.setState({homeless:!this.state.homeless})
+      break;
+      case 'food': this.setState({food:!this.state.food})
+      break;
+      case 'education': this.setState({education:!this.state.education})
+      break;
+      case 'health': this.setState({health:!this.state.health})
+      break;
+      case 'equality': this.setState({equality:!this.state.equality})
+      break;
+      case 'religious': this.setState({religious:!this.state.religious})
+      break;
+      case 'crisis': this.setState({crisis:!this.state.crisis})
+      break;
+      case 'arts': this.setState({arts:!this.state.arts})
+      break;
+      case 'energy': this.setState({energy:!this.state.energy})
+      break;
+      case 'conservation': this.setState({conservation:!this.state.conservation})
+      break;
+      case 'peace': this.setState({peace:!this.state.peace})
+      break;
+      case 'refugees': this.setState({refugees:!this.state.refugees})
+      break;
+    }
+  }
+
  render(){
     return (
       <ScrollView>
       <View style={styles.container}> 
      
             <Text style={{color:'#01b0b0',fontSize:25, marginTop: 20, textAlign: 'center'}}>What causes do you care about?</Text>
-            <Grid style={{marginTop: 200}}></Grid>
+            <Grid changeState={this.causeSelected} style={{marginTop: 200}}></Grid>
             <View style={{flexDirection:'row',margin:5,marginBottom:20}}>
             <ClassicButton
                     
